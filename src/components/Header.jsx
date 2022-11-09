99876d4c6f4c90979a17b62dc8c3c5a0b26080ab
s827 <dskumar827@gmail.com>
2022-11-09 14:48:28 +0530

    image loading problem, loaded image locally

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import TrollFace from '../images/troll-face.png'
 
 export default function Header() {
     return (
         <div className='header-div'>
             <div className='inner-div'>
-                <img className='header-icon' src='https://thenounproject.com/api/private/icons/105264/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABjaqJhqlq_dvCgYE1ZEBmK7nt-m5IZeyVtI0fTLv2Xwc5hrta4g9XXXkbMzP8oI_AyYZMNiyAqsphktCOYpyFv_2Dd4g%3D%3D' />
+                <img className='header-icon' src={TrollFace} />
                 <h1 className='header-title'>Meme Generator</h1>
             </div>
             <div className='header-subtitle'>
