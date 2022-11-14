e69cb42c3ac62df7ae9f066ffc14dd22f99e997c
s827 <dskumar827@gmail.com>
2022-11-15 02:07:47 +0530

    Added randomly chosen images when button is clicked

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import Frame from '../images/frame.png'
+import MemeData from '../memesData'
 export default function Meme() {
+    const[memeImage, setMemeImage] = React.useState('')
+    function getMemeImage() {
+        const imgDir = MemeData.data.memes
+        const memeIndex = Math.floor(Math.random() * imgDir.length)
+        const imgURL = imgDir[memeIndex].url
+        setMemeImage(imgURL)
+    }
     return (
-        <div className='meme-div'>
-            <div className='meme-textInput'>
-                <input 
-                    className='firstTextInput'
-                    placeholder='Shut Up' 
-                />
-                <input 
-                    placeholder='And take my money'
-                />
+        <main>
+            <div className='meme-div'>
+                <div className='meme-textInput'>
+                    <input 
+                        className='firstTextInput'
+                        placeholder='Shut Up' 
+                    />
+                    <input 
+                        placeholder='And take my money'
+                    />
+                </div>
+                <button 
+                    className='meme-button'
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image <img className='meme-frame' src={Frame} />
+                </button>
             </div>
-            <button className='meme-button'>Get a new meme image <img className='meme-frame' src={Frame} /></button>
-        </div>
+            <img 
+                src={memeImage}
+                className='meme-image' 
+            />
+        </main>
     )
 }
